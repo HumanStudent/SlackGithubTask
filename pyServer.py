4fcbcaa311d310da44498cc109d41919af66a444
HumanStudent <salesforce.kavmahve.task@gmail.com>
2020-10-31 21:34:21 +0200

    test00

--- pyServer.py
+++ pyServer.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hello Its Home"
+    return "Hello Its Home welcome"
 
 @app.route('/github', methods=['POST'])
 def get_github_notafication():
