929b41517e54f187510c427d227f5956e0bd8a21
HumanStudent <salesforce.kavmahve.task@gmail.com>
2020-11-06 12:43:05 +0200

    json stuff 4

--- pyServer.py
+++ pyServer.py
@@ -24,7 +24,7 @@ webhook = Webhook(app) # Defines '/postreceive' endpoint
 @app.route('/')
 def get_github_notafication():
     if request.headers['Content-Type'] == 'application/json':
-        data = json.dumps(request.json, indent=2)
+        data = json.dumps(request.json)
         # print(data)
         print("Got push with: {0}".format(data))
         return data
